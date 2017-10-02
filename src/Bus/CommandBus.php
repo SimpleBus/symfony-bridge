853c340a2730efb356b90ad72e944267fb8fafac
Ruud Kamphuis <ruudk@users.noreply.github.com>
2017-10-02 10:03:07 +0200

    Add `CommandBus` and `EventBus` classes to support Symfony Autowiring

---
+++ src/Bus/CommandBus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SimpleBus\SymfonyBridge\Bus;
+
+use SimpleBus\Message\Bus\Middleware\MessageBusSupportingMiddleware;
+
+class CommandBus extends MessageBusSupportingMiddleware
+{
+
+}
