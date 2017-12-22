8d53e8dd5d611c84b8f5b81195fe3b784a4ca2f9
Cliff Odijk <cliff@jcid.nl>
2017-12-22 09:54:31 +0100

    Add symfony/framework-bundle to the requirements

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,3 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 require __DIR__ . '/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader('class_exists');
-
-// Polyfill PHPUnit 6.0 both ways
-if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-} elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
